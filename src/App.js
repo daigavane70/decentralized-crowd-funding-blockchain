13bec2927992e9cc282008f5c6efd553fa849ea6
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-11-22 20:24:04 +0530

    basic project initiation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Layout, Menu } from "antd";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout hasSider>
+      <Sider
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          position: "fixed",
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
+      >
+        <div className="p-4 text-white">Logo</div>
+        <Menu
+          mode="inline"
+          theme="dark"
+          items={[
+            { label: "Home", key: "home" },
+            { label: "My Startups", key: "mystartups" },
+            { label: "Profile", key: "profile" },
+          ]}
+        ></Menu>
+      </Sider>
+      <Layout className="site-layout" style={{ marginLeft: 200 }}>
+        <Header className="site-layout-background flex" style={{ padding: 0 }}>
+          <div className="text-3xl font-light my-auto text-blue-500">
+            Crowd Funding Application
+          </div>
+        </Header>
+        <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, textAlign: "center" }}
+          ></div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Decentralized crowd funding and fraud prevention application using
+          Blockchain
+        </Footer>
+      </Layout>
+    </Layout>
   );
 }
 
