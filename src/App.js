a1fdbab815bc8740952b4b7907d829220d6d18e8
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-11-23 00:37:21 +0530

    home and invest page completed

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import { Layout, Menu } from "antd";
 import { Content, Footer, Header } from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
 import React from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import MyStartups from "./pages/MyStartups";
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <Layout hasSider>
       <Sider
@@ -21,24 +27,36 @@ function App() {
         <Menu
           mode="inline"
           theme="dark"
-          items={[
-            { label: "Home", key: "home" },
-            { label: "My Startups", key: "mystartups" },
-            { label: "Profile", key: "profile" },
-          ]}
+          items={navBar.map((nav) => {
+            return {
+              label: nav.label,
+              key: nav.key,
+              onClick: () => {
+                navigate(nav.link);
+              },
+            };
+          })}
         ></Menu>
       </Sider>
       <Layout className="site-layout" style={{ marginLeft: 200 }}>
         <Header className="site-layout-background flex" style={{ padding: 0 }}>
-          <div className="text-3xl font-light my-auto text-blue-500">
+          <div className="text-3xl my-auto text-cyan-400">
             Crowd Funding Application
           </div>
         </Header>
-        <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+        <Content style={{ margin: "0px 16px 0", overflow: "initial" }}>
           <div
             className="site-layout-background"
             style={{ padding: 24, textAlign: "center" }}
           ></div>
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/profile" element={<Profile></Profile>}></Route>
+            <Route
+              path="/mystartups"
+              element={<MyStartups></MyStartups>}
+            ></Route>
+          </Routes>
         </Content>
         <Footer style={{ textAlign: "center" }}>
           Decentralized crowd funding and fraud prevention application using
@@ -48,5 +66,15 @@ function App() {
     </Layout>
   );
 }
+
+const navBar = [
+  {
+    label: "Home",
+    key: "home",
+    link: "/",
+  },
+  { label: "My Startups", key: "mystartups", link: "/mystartups" },
+  { label: "Profile", key: "profile", link: "/profile" },
+];
 
 export default App;
