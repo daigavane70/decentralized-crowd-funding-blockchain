d04078ee07400b031d8bbea10a31b78c9cba0084
Bhavana <bbafna@amazon.com>
2023-02-16 13:34:41 +0530

    added new fields

--- src/pages/StartupDetails.js
+++ src/pages/StartupDetails.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-export default function StartupDetails() {
-  return <div>StartupDetails</div>;
-}
+const StartupDetails = (props) => {
+  const [startup, setStartup] = useState({});
+
+  useEffect(() => {
+    const fetchStartup = async () => {
+      // fetch startup data using the id from the props
+      const response = await fetch(`https://api.example.com/startups/${props.id}`);
+      const data = await response.json();
+      setStartup(data);
+    };
+    fetchStartup();
+  }, [props.id]);
+
+  return (
+    <div className="startup-details">
+      <h2>{startup.name}</h2>
+      <p>Valuation: {startup.valuation}</p>
+      <p>Segment: {startup.segment}</p>
+      <p>CEO: {startup.ceo}</p>
+      <p>Company Size: {startup.companySize}</p>
+      <p>Founded in: {startup.foundedIn}</p>
+    </div>
+  );
+};
+
+export default StartupDetails;
+
+
